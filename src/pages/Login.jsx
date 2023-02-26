04ffa2dabfa507b47ef0bcff2f6292a489126585
thaosuong01 <maikhoi10112001@gmail.com>
2023-02-26 19:40:36 +0700

    validate form login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import {
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+} from "@mui/material";
 import TextField from "@mui/material/TextField";
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+
+const SigninSchema = yup.object().shape({
+  username: yup.string().required(),
+  password: yup.string().required(),
+});
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(SigninSchema) });
+
+  const onSubmit = (data) => {
+    console.log(JSON.stringify(data));
+  };
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
   return (
     <>
       <div className="bg-white py-[10px] px-[20px]">
@@ -10,23 +42,46 @@ const Login = () => {
         </h1>
         <div className="flex justify-center items-center flex-col gap-0">
           <form
-            action=""
+            onSubmit={handleSubmit(onSubmit)}
             className="bg-[#eee] w-2/5 flex justify-center flex-col gap-8 items-center p-6"
           >
             <div className="w-full">
               <TextField
                 className="w-full bg-white"
                 id="demo-helper-text-misaligned-no-helper"
-                label="Name"
+                label="Username"
+                {...register("username")}
               />
+              {errors.username && (
+                <p className="text-[#d53532]">{errors.username.message}</p>
+              )}
             </div>
-            <div className="w-full">
-              <TextField
+            <FormControl className="w-full" variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
                 className="w-full bg-white"
-                id="demo-helper-text-misaligned-no-helper"
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
                 label="Password"
+                {...register("password")}
               />
-            </div>
+              {errors.password && (
+                <p className="text-[#d53532]">{errors.password.message}</p>
+              )}
+            </FormControl>
             <div className="justify-start flex w-full gap-3 cursor-pointer">
               <input type="checkbox" name="" id="" />
               <span className="text-[#555]">Remember username</span>
